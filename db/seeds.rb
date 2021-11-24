8ad540ab5d314374acad7c269ec67da689739bad
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-25 00:32:39 +0100

    adds seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do 
+  book = Book.create(author: Faker::Book.author, title: Faker::Book.title)
+  puts book;
+end
