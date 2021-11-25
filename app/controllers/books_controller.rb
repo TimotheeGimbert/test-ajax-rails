c3d72c2855e40f237b73b0bf190f2e828aa682e4
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-25 01:08:14 +0100

    adds view index and book adding form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,18 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
   end
+
+  def create
+    @book = Book.create(book_params)
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.permit(:title, :author)
+  end
+  
 end
